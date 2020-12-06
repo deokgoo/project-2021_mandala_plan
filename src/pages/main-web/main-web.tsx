652ae6f3bbbfd7e38bd5ac6b5d408d4f13dfc546
deokgoo <kkddgg1001@gmail.com>
2020-12-06 21:20:24 +0900

    fix: typescript build error

--- src/pages/main-web/main-web.tsx
+++ src/pages/main-web/main-web.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../../components/header';
 import MandalaWeb from '../../components/mandala-web';
 import styles from './main-web.module.scss';

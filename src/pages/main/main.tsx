6a9e1f991d5ef8e7ed2f78a200c692e45d033844
deokgoo <kkddgg1001@gmail.com>
2020-12-14 17:47:53 +0900

    fix: performance fix (view change web => mobile)

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import UseMain from './hooks';
 import MainWeb from '../main-web';
 import MainMobile from '../main-mobile';
+import { viewTypeEnum } from './hooks/type';
 
 const Main = () => {
-  const mobileWidth = 576;
-  const { windowDimensions } = UseMain();
-  return (windowDimensions.width>mobileWidth ? <MainWeb /> : <MainMobile /> )
+  const { windowViewType } = UseMain();
+  return (windowViewType===viewTypeEnum.web ? <MainWeb /> : <MainMobile /> )
 }
 
 export default Main;

3a89a89428a30b43d361a597bfe6ae82bdd9353e
deokgoo <kkddgg1001@gmail.com>
2020-12-19 23:59:49 +0900

    fix: rename

--- src/redux/mandala/hooks/index.ts
+++ src/redux/mandala/hooks/index.ts
@@ -1,7 +1,9 @@
 import useMandalaUnitUpdate from './use-mandala-unit-update';
 import useMandalaUnitSelector from './use-mandala-unit-selector';
+import useMandalaDreamSelector from './use-mandala-dream-selector';
 
 export default {
-  useMandalaSelector: useMandalaUnitSelector,
-  useMandalaUpdate: useMandalaUnitUpdate,
+  useMandalaUnitSelector,
+  useMandalaUnitUpdate,
+  useMandalaDreamSelector,
 };
